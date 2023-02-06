0e389891f06019b634317222540056b6bd6dda65
StoneT2000 <stonezt2019@gmail.com>
2023-02-06 14:08:30 -0800

    working code

--- examples/tutorials/reinforcement-learning/rllib_ppo_liftcube_state.py
+++ examples/tutorials/reinforcement-learning/rllib_ppo_liftcube_state.py
@@ -10,20 +10,14 @@ Note: rllib only supports a single observation modality:
 """
 import gym
 import argparse
-import functools
 import logging
-import operator
-import os
 from sys import platform
 import mani_skill2.envs
-
+import os.path as osp
 from mani_skill2.utils.wrappers import RecordEpisode
 
 import ray
-import yaml
 from ray import tune
-from ray.rllib.models import ModelCatalog
-from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
 from ray.tune.registry import register_env
 from ray.rllib.algorithms.ppo import PPOConfig
 # ray.init(local_mode=True)
@@ -32,26 +26,26 @@ ray.init(ignore_reinit_error=True)
 
 # Defines a continuous, infinite horizon, task where done is always False
 # unless a timelimit is reached.
-class ContinuousTaskWrapper(gym.Wrapper):
-    def __init__(self, env, max_episode_steps: int) -> None:
-        super().__init__(env)
-        self._elapsed_steps = 0
-        self._max_episode_steps = max_episode_steps
+# class ContinuousTaskWrapper(gym.Wrapper):
+#     def __init__(self, env, max_episode_steps: int) -> None:
+#         super().__init__(env)
+#         self._elapsed_steps = 0
+#         self._max_episode_steps = max_episode_steps
 
-    def reset(self):
-        self._elapsed_steps = 0
-        return super().reset()
+#     def reset(self):
+#         self._elapsed_steps = 0
+#         return super().reset()
 
-    def step(self, action):
-        ob, rew, done, info = super().step(action)
-        self._elapsed_steps += 1
-        if self._elapsed_steps >= self._max_episode_steps:
-            done = True
-            info["TimeLimit.truncated"] = True
-        else:
-            done = False
-            info["TimeLimit.truncated"] = False
-        return ob, rew, done, info
+#     def step(self, action):
+#         ob, rew, done, info = super().step(action)
+#         self._elapsed_steps += 1
+#         if self._elapsed_steps >= self._max_episode_steps:
+#             done = True
+#             info["TimeLimit.truncated"] = True
+#         else:
+#             done = False
+#             info["TimeLimit.truncated"] = False
+#         return ob, rew, done, info
 
 
 # A simple wrapper that adds a is_success key which SB3 tracks
@@ -66,17 +60,13 @@ def env_creator(env_config):
     obs_mode = env_config["obs_mode"]
     max_episode_steps = env_config["max_episode_steps"]
     import mani_skill2.envs
-
+    print("CONFIG", env_config)
     env = gym.make(
         env_id,
         obs_mode=obs_mode,
         reward_mode=env_config["reward_mode"],
         control_mode=env_config["control_mode"],
     )
-    # For training, we regard the task as a continuous task with infinite horizon.
-    # you can use the ContinuousTaskWrapper here for that
-    if max_episode_steps is not None:
-        env = ContinuousTaskWrapper(env, max_episode_steps)
     if env_config["record_dir"] is not None:
         env = SuccessInfoWrapper(env)
         env = RecordEpisode(
@@ -112,7 +102,7 @@ def parse_args():
     parser.add_argument(
         "--total-timesteps",
         type=int,
-        default=300_000,
+        default=250_000,
         help="Total timesteps for training",
     )
     parser.add_argument(
@@ -149,28 +139,45 @@ def main():
             "control_mode": control_mode,
             "record_dir": None
         },
+        "evaluation_interval": 100,
+        "evaluation_duration": 5,
+        "evaluation_config": {
+            "env_config": {
+                "env_id": args.env_id,
+                "max_episode_steps": args.max_episode_steps,
+                "obs_mode": "state",
+                "reward_mode": "dense",
+                "control_mode": control_mode,
+                "record_dir": "videos"
+            },
+            "no_done_at_end": False,
+        },
         "num_gpus": 1,
-        # "remote_worker_envs": True,
         "num_workers": num_envs,
         "num_envs_per_worker": 1,
         "num_cpus_per_worker": 1,
         "rollout_fragment_length": 100,
         "train_batch_size": rollout_size,
         "sgd_minibatch_size": 400,
+        "batch_mode": "truncate_episodes",
+        # For training, we regard the task as a continuous task with infinite horizon, which has generally better performance
+        "no_done_at_end": True,
         "num_sgd_iter": 15,
         "kl_target": 0.05,
         "gamma": 0.85,
         "framework": "torch",
     }
-    stop = {"training_iteration": args.total_timesteps}
-    # if resume is not None:
-    #     assert restore_checkpoint is not None, "Error: When resuming must provide explicit path to checkpoint"
+    training_iterations = args.total_timesteps // rollout_size
+    stop = {"training_iteration": training_iterations}
+
+    if args.seed is not None:
+        config["seed"] = args.seed
 
     results = tune.run(
         "PPO",
         config=config,
         verbose=2,
-        name="test",
+        name="test3",
         local_dir=args.log_dir,
         checkpoint_freq=100,
         stop=stop,
