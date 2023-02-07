101e0924ce1f601e57cf8d73eee62a297f1b197d
StoneT2000 <stonezt2019@gmail.com>
2023-02-06 18:57:24 -0800

    rllib work

--- examples/tutorials/reinforcement-learning/README.md
+++ examples/tutorials/reinforcement-learning/README.md
@@ -1,11 +1,18 @@
 # Reinforcement Learning with ManiSkill2
 
-This contains single-file implementations that solve with LiftCube environment with rgbd or state observations. All scripts contain the same arguments and can be run as so
+This contains single-file implementations that solve with LiftCube environment with rgbd or state observations. All scripts contain mostly the same arguments, see the following sections on how to run the scripts for each supported RL library
+
+Currently supported:
+
+- [Stable Baselines 3](#stable-baselines-3)
+- [RLlib](#rllib)
+
+## Stable Baselines 3
 
 
 ```
 # Training
-python sb3_ppo_liftcube_rgbd.py
+python sb3_ppo_liftcube_rgbd.py --log-dir=logs
 
 # Evaluation
 python sb3_ppo_liftcube_rgbd.py --eval --model-path=path/to/model
@@ -14,3 +21,17 @@ python sb3_ppo_liftcube_rgbd.py --eval --model-path=path/to/model
 Pass in `--help` for more options (e.g. logging, number of parallel environmnets, whether to use ManiSkill2 Vectorized Environments or not etc.)
 
 For Stable Baselines 3, you need to install `stable_baselines3` as well as `tensorboard`.
+
+## RLlib
+
+```
+# Training
+python rllib_ppo_liftcube_state.py --log-dir=logs --exp-name=test
+
+# Evaluation
+python rllib_ppo_liftcube_state.py --eval --model-path=path/to/model
+```
+
+Pass in `--help` for more options (e.g. logging, number of parallel environmnets, whether to use ManiSkill2 Vectorized Environments or not etc.)
+
+By default the code uses a PyTorch backend, you can easily edit the script to try a different backend.
--- examples/tutorials/reinforcement-learning/rllib_ppo_liftcube_state.py
+++ examples/tutorials/reinforcement-learning/rllib_ppo_liftcube_state.py
@@ -8,44 +8,47 @@ This example requires ray to be installed with rllib support, and pytorch to be 
 
 Note: rllib only supports a single observation modality:
 """
-import gym
 import argparse
 import logging
-from sys import platform
-import mani_skill2.envs
 import os.path as osp
+from sys import platform
+
+import gym
+import ray
+import ray.rllib.algorithms.ppo as ppo
+from ray import tune
+from ray.rllib.algorithms.ppo import PPOConfig
+from ray.tune.registry import register_env
+
+import mani_skill2.envs
 from mani_skill2.utils.wrappers import RecordEpisode
 
-import ray
-from ray import tune
-from ray.tune.registry import register_env
-from ray.rllib.algorithms.ppo import PPOConfig
 # ray.init(local_mode=True)
 ray.init(ignore_reinit_error=True)
 
 
 # Defines a continuous, infinite horizon, task where done is always False
 # unless a timelimit is reached.
-# class ContinuousTaskWrapper(gym.Wrapper):
-#     def __init__(self, env, max_episode_steps: int) -> None:
-#         super().__init__(env)
-#         self._elapsed_steps = 0
-#         self._max_episode_steps = max_episode_steps
+class ContinuousTaskWrapper(gym.Wrapper):
+    def __init__(self, env, max_episode_steps: int) -> None:
+        super().__init__(env)
+        self._elapsed_steps = 0
+        self._max_episode_steps = max_episode_steps
 
-#     def reset(self):
-#         self._elapsed_steps = 0
-#         return super().reset()
+    def reset(self):
+        self._elapsed_steps = 0
+        return super().reset()
 
-#     def step(self, action):
-#         ob, rew, done, info = super().step(action)
-#         self._elapsed_steps += 1
-#         if self._elapsed_steps >= self._max_episode_steps:
-#             done = True
-#             info["TimeLimit.truncated"] = True
-#         else:
-#             done = False
-#             info["TimeLimit.truncated"] = False
-#         return ob, rew, done, info
+    def step(self, action):
+        ob, rew, done, info = super().step(action)
+        self._elapsed_steps += 1
+        if self._elapsed_steps >= self._max_episode_steps:
+            done = True
+            info["TimeLimit.truncated"] = True
+        else:
+            done = False
+            info["TimeLimit.truncated"] = False
+        return ob, rew, done, info
 
 
 # A simple wrapper that adds a is_success key which SB3 tracks
@@ -55,25 +58,27 @@ class SuccessInfoWrapper(gym.Wrapper):
         info["is_success"] = info["success"]
         return ob, rew, done, info
 
+
 def env_creator(env_config):
     env_id = env_config["env_id"]
     obs_mode = env_config["obs_mode"]
     max_episode_steps = env_config["max_episode_steps"]
     import mani_skill2.envs
-    print("CONFIG", env_config)
+
     env = gym.make(
         env_id,
         obs_mode=obs_mode,
         reward_mode=env_config["reward_mode"],
         control_mode=env_config["control_mode"],
     )
+    if max_episode_steps is not None:
+        env = ContinuousTaskWrapper(env, max_episode_steps=max_episode_steps)
     if env_config["record_dir"] is not None:
         env = SuccessInfoWrapper(env)
         env = RecordEpisode(
             env, env_config["record_dir"], info_on_video=True, render_mode="cameras"
         )
     return env
-
 
 
 def parse_args():
@@ -102,7 +107,7 @@ def parse_args():
     parser.add_argument(
         "--total-timesteps",
         type=int,
-        default=250_000,
+        default=1_000_000,
         help="Total timesteps for training",
     )
     parser.add_argument(
@@ -110,6 +115,11 @@ def parse_args():
         type=str,
         default="logs",
         help="path for where logs, checkpoints, and videos are saved",
+    )
+    parser.add_argument(
+        "--exp-name",
+        type=str,
+        help="an experiment name",
     )
     parser.add_argument(
         "--eval", action="store_true", help="whether to only evaluate policy"
@@ -134,23 +144,24 @@ def main():
         "env_config": {
             "env_id": args.env_id,
             "max_episode_steps": args.max_episode_steps,
-            "obs_mode": "state",
-            "reward_mode": "dense",
+            "obs_mode": obs_mode,
+            "reward_mode": reward_mode,
             "control_mode": control_mode,
-            "record_dir": None
+            "record_dir": None,
         },
-        "evaluation_interval": 100,
+        "evaluation_interval": 16000,
         "evaluation_duration": 5,
         "evaluation_config": {
             "env_config": {
                 "env_id": args.env_id,
-                "max_episode_steps": args.max_episode_steps,
-                "obs_mode": "state",
-                "reward_mode": "dense",
+                "max_episode_steps": None,
+                "obs_mode": obs_mode,
+                "reward_mode": reward_mode,
                 "control_mode": control_mode,
-                "record_dir": "videos"
+                "record_dir": "videos",
             },
             "no_done_at_end": False,
+            "explore": False,
         },
         "num_gpus": 1,
         "num_workers": num_envs,
@@ -165,6 +176,12 @@ def main():
         "num_sgd_iter": 15,
         "kl_target": 0.05,
         "gamma": 0.85,
+        "lambda_": 0.95,
+        "clip_param": 0.2,
+        "model": {
+            "fcnet_hiddens": [256, 256],
+            "fcnet_activation": "relu",
+        },
         "framework": "torch",
     }
     training_iterations = args.total_timesteps // rollout_size
@@ -173,15 +190,30 @@ def main():
     if args.seed is not None:
         config["seed"] = args.seed
 
-    results = tune.run(
-        "PPO",
-        config=config,
-        verbose=2,
-        name="test3",
-        local_dir=args.log_dir,
-        checkpoint_freq=100,
-        stop=stop,
-    )
+    if args.eval:
+        # config["num_workers"] = 0
+        algo = ppo.PPO(config=config)
+        algo.load_checkpoint(args.model_path)
+    else:
+        assert (
+            args.exp_name is not None
+        ), "Experiment name is required for training. Specify with --exp-name"
+        results = tune.run(
+            "PPO",
+            config=config,
+            verbose=2,
+            name=args.exp_name,
+            local_dir=args.log_dir,
+            checkpoint_freq=16,
+            checkpoint_at_end=True,
+            stop=stop,
+        )
+        # get latest checkpoint
+        latest_checkpoint = results.get_best_checkpoint(results.trials[0], mode="max")
+        algo = ppo.PPO(config=config)
+        algo.load_checkpoint(latest_checkpoint)
+    results = algo.evaluate()
+    print(results)
 
 
 if __name__ == "__main__":
